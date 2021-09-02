7d960a51f0065f7b1a5bcf75f7805e4b95a8501a
Alejandro Rivas <alejandro.rivas@go.tarleton.edu>
2021-09-02 09:09:50 -0500

    C2

---
+++ Calculator/src/main/java/com/mycompany/calculator/Calculator.java
@@ -0,0 +1,28 @@
+package com.mycompany.calculator;
+
+public class Calculator {
+    
+    public static void addition(double x, double y) {
+        double sum = 0;
+        sum = x + y;
+        System.out.println(sum);
+    }
+    
+    public static void subtraction(double x, double y) {
+        double difference = 0;
+        difference = x - y;
+        System.out.println(difference);
+    }
+    
+    public static void multiplication(double x, double y) {
+        double product = 0;
+        product = x * y;
+        System.out.println(product);
+    }
+    
+    public static void division(double x, double y) {
+        double qoutient = 0;
+        qoutient = x / y;
+        System.out.println(qoutient);
+    }
+}
---
+++ Calculator/src/main/java/com/mycompany/calculator/Main.java
@@ -0,0 +1,18 @@
+package com.mycompany.calculator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        Calculator calc = new Calculator();
+//        calc.addition(3,4);
+//        calc.subtraction(10,2);
+//        calc.multiplication(3,8);
+//        calc.division(28,2);
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter expression");
+        String expression = sc.nextLine();
+        //System.out.println("%.8f", answer);
+    }
+}
